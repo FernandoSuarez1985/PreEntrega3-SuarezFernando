e1edd65c8cb341c6f031e8d7c4c6e73e4c15e40f
FernandoSuarez1985 <106759991+FernandoSuarez1985@users.noreply.github.com>
2023-03-06 12:06:53 -0300

    primero

--- main.js
+++ main.js
@@ -12,5 +12,20 @@ function agregarProducto (nombre, precio) {
     
     carrito.push(producto);
 
-    const listaCarrito = document.getElementById("")
+    const listaCarrito = document.getElementById("lista-carrito");
+    const elemento = document.createElement("li");
+    elemento.innerText = `${nombre} - $${precio}`;
+    listaCarrito.appendChild(elemento);
+
+    total += precio;
+    const totalCarrito = document.getElementById("total-carrito");
+    totalCarrito.innerText = total.toFixed();
+
+}
+
+function finalizarCompra() {
+    var total = document.getElementById("total-carrito").innerText;
+    localStorage.setItem("compra", "Total: " + total);
+    alert("Gracias por su compra. \n\n" + "Total: " + total);
+    location.reload();
 }
