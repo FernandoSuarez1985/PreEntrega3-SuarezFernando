1f6ca2842650d540e3ae8fcff9a7b58a11aff725
FernandoSuarez1985 <106759991+FernandoSuarez1985@users.noreply.github.com>
2023-03-06 09:41:49 -0300

    primer

---
+++ main.js
@@ -0,0 +1,16 @@
+let carrito = [] ;
+let total = 0;
+
+function agregarProducto (nombre, precio) {
+    
+    const producto = {
+
+        nombre: nombre,
+        precio: precio,
+
+    };
+    
+    carrito.push(producto);
+
+    const listaCarrito = document.getElementById("")
+}
